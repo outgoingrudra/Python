94dedb92b87e74782e540aeffca78ff6fc53c6ae
Rudra Verma <rudraverma781@gmail.com>
2025-08-18 05:51:27 +0530

    work

---
+++ random/num_guess.py
@@ -0,0 +1,11 @@
+import random
+
+number = random.randint(1, 100)
+guess = None
+while guess != number:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+print("Correct! The number was", number)
