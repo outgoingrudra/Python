b233c7a578421ee52414d34f439b5cf543152659
Rudra Verma <rudraverma781@gmail.com>
2025-08-24 07:28:53 +0530

    work

---
+++ numpy/statistics.py
@@ -0,0 +1,8 @@
+arr = np.array([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+print("Mean:", np.mean(arr))
+print("Median:", np.median(arr))
+print("Variance:", np.var(arr))
+print("Std Deviation:", np.std(arr))
