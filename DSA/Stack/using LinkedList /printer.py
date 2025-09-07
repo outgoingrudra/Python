a7eea27e991c69387ecd230c686b8edd9fe6ddc6
Rudra Verma <rudraverma781@gmail.com>
2025-09-07 17:29:28 +0530

    work

---
+++ DSA/Stack/using LinkedList /printer.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,v):
+        self.v=v
+        self.n=None
+
+class Stack:
+    def __init__(self):
+        self.t=None
+    def push(self,x):
+        n=Node(x); n.n=self.t; self.t=n
+    def show(self):
+        t=self.t
+        while t: print(t.v,end=" "); t=t.n
+
+s=Stack()
+s.push(1); s.push(2); s.push(3)
+s.show()
